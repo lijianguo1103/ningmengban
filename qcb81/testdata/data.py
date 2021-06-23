f8fdbf3348e33cbd283abb49f05e7b4643b2697a
lijianguo1103 <73392269+lijianguo1103@users.noreply.github.com>
2021-06-23 23:22:00 +0800

    Delete qcb81 directory

--- qcb81/testdata/data.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File : data.py 
-# @Software: PyCharm
-# @Time : 2021/6/21 21:45
-# @Author : 李建国
-# @AuthorEmail : 1650408189@qq.com
-
-data_test = {"url": "http://erp.lemfix.com/login.html", "name": "test123", "password": "123456", "key": "448"}
